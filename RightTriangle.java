b12eb4ee2b9db6a723fb15a3136b9d29547b696f
AustinPotts <pottsevolvements@gmail.com>
2020-06-28 21:54:49 -0400

    Right Triangle

---
+++ RightTriangle.java
@@ -0,0 +1,16 @@
+
+public class RightTriangle {
+    public static boolean main(Int args[]) {
+        Boolean b1 = (true);  
+        Boolean b2 = (false);
+        int x = 3;  
+
+        if(args[0] + args[1] + args[3] / x == 0) {
+            System.out.println(b2);
+        }
+
+        else {
+            System.out.println(b1);
+        }
+    }
+}
