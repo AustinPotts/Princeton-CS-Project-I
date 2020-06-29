dcb072e8175890a84934441cdfbe323abcfc356f
AustinPotts <pottsevolvements@gmail.com>
2020-06-28 21:17:13 -0400

    Hello, Goodbye

---
+++ HelloGoodbye.java
@@ -0,0 +1,6 @@
+
+public class HelloGoodbye {
+    public static void main(String args[]) {
+        System.out.print("Hello " + args[0] + " and " + args[1] + "\n" + "Goodbye " + args[1] + " and " + args[0]);
+    }
+}
